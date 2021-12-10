70d214c86269e00843e129f4afd06657983d6a59
R-Prince <reneprince93@icloud.com>
2021-12-10 15:42:58 +0000

    Add Pah model

--- pah/models.py
+++ pah/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from sku.models import Product
 
-# Create your models here.
+
+# Pah Products and Prices
+class Pah(models.Model):
+    sku = models.ForeignKey(Product, on_delete=models.CASCADE)
+    sku_code = models.CharField(max_length=10, null=False, blank=False)
+    price = models.IntegerField(null=False, blank=False)
+    vat = models.IntegerField(max_length=14, null=False, blank=False)
+    channel = models.IntegerField(max_length=14, null=False, blank=False)
+    sku_type = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.sku
